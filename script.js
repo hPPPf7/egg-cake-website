c50ade228792279c197988bc5b105b2b374f47c0
hPPPf7 <112109254+hPPPf7@users.noreply.github.com>
2025-06-09 15:37:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("scrollToMenu").addEventListener("click", function () {
+  document.getElementById("menuSection").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("toggleFlavors").addEventListener("click", function () {
+  const allFlavors = document.getElementById("allFlavors");
+  if (allFlavors.style.display === "flex") {
+    allFlavors.style.display = "none";
+    this.textContent = "查看全部口味";
+  } else {
+    allFlavors.style.display = "flex";
+    this.textContent = "收起口味列表";
+  }
+});
