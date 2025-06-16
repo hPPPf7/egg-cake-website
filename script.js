687a039adb16cc1db51e93f0d6bf8d04868887ad
hPPPf7 <meliwawa9107@gmail.com>
2025-06-17 06:18:11 +0800

    0617初版

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById("scrollToMenu").addEventListener("click", function () {
   document.getElementById("menuSection").scrollIntoView({ behavior: "smooth" });
 });
@@ -12,3 +13,19 @@ document.getElementById("toggleFlavors").addEventListener("click", function () {
     this.textContent = "收起口味列表";
   }
 });
+=======
+document.getElementById("scrollToMenu").addEventListener("click", function () {
+  document.getElementById("menuSection").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("toggleFlavors").addEventListener("click", function () {
+  const allFlavors = document.getElementById("allFlavors");
+  if (allFlavors.style.display === "flex") {
+    allFlavors.style.display = "none";
+    this.textContent = "查看全部口味";
+  } else {
+    allFlavors.style.display = "flex";
+    this.textContent = "收起口味列表";
+  }
+});
+>>>>>>> a9141e6 (第一次上傳)
